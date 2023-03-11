dcb2333cdfba3901fbdfcbddea1cb67fbd7144ea
lea.tuan021211 <lea.tuan021211@gmail.com>
2023-03-12 04:34:43 +0700

    Create Authencation

---
+++ BEComicWeb/Controllers/StoryController.cs
@@ -0,0 +1,86 @@
+﻿using BEComicWeb.Interface.StoryInterface;
+using BEComicWeb.Model.StoryModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BEComicWeb.Responsitory.StoryResponsitory;
+
+namespace BEComicWeb.Controllers
+{
+    [Route("api/Story")]
+    [ApiController]
+    public class StoryController : ControllerBase
+    {
+        private readonly IStoryResponse _IStoryResponse;
+
+        public StoryController(IStoryResponse IStoryRes)
+        {
+            _IStoryResponse = IStoryRes;
+        }
+
+        // GET: api/Story
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Stories>>> Get()
+        {
+            return await Task.FromResult(_IStoryResponse.GetStoryDetails());
+        }
+
+        // GET api/Story/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Stories>> Get(string id)
+        {
+            var Stories = await Task.FromResult(_IStoryResponse.GetStoryDetail(id));
+            if (Stories == null)
+            {
+                return NotFound();
+            }
+            return Stories;
+        }
+
+        // POST api/Story
+        [HttpPost]
+        public async Task<ActionResult<Stories>> Post(Stories Story)
+        {
+            _IStoryResponse.AddStory(Story);
+            return await Task.FromResult(Story);
+        }
+
+        // PUT api/Story/{id}
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Stories>> Put(string id, Stories Story)
+        {
+            if (id != Story.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _IStoryResponse.UpdateStory(Story);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return await Task.FromResult(Story);
+        }
+
+        // DELETE api/Story/{id}
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Stories>> Delete(string id)
+        {
+            var Story = _IStoryResponse.DeleteStory(id);
+            return await Task.FromResult(Story);
+        }
+
+        private bool StoryExists(string id)
+        {
+            return _IStoryResponse.CheckStoryExists(id);
+        }
+    }
+}
---
+++ BEComicWeb/Migrations/20230304160648_stories.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BEComicWeb.Migrations
+{
+    public partial class stories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stories",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "varchar(60)", unicode: false, maxLength: 60, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Alias = table.Column<string>(type: "varchar(60)", unicode: false, maxLength: 60, nullable: true),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Keyword = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Follows = table.Column<int>(type: "int", unicode: false, nullable: false),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", unicode: false, nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime2", unicode: false, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stories", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stories");
+        }
+    }
+}
